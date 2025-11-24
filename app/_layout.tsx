847b3ea6d7d4018f1f0e4d0e8b01ea06531cb0b8
Patrick Royce Perez <patrickperez.iformatlogic@gmail.com>
2025-11-24 17:00:38 +0800

    Created Modal for Orders

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,6 +3,8 @@ import { ClerkProvider } from '@clerk/clerk-expo';
 import { tokenCache } from "@clerk/clerk-expo/token-cache";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Stack } from "expo-router";
+import { Provider as PaperProvider } from "react-native-paper";
+
 
 const queryClient = new QueryClient()
 
@@ -11,16 +13,18 @@ export default function RootLayout() {
 
     <ClerkProvider tokenCache={tokenCache}>
       <QueryClientProvider client={queryClient}>
-        <ThemeProvider>
-            <Stack screenOptions={{ headerShown: false }}>
-              <Stack.Screen
-                name="(tabs)"
-                options={{
-                  title: "Home"
-                }}
-              />
-            </Stack>
-        </ThemeProvider>
+          <ThemeProvider>
+            <PaperProvider>
+              <Stack screenOptions={{ headerShown: false }}>
+                <Stack.Screen
+                  name="(tabs)"
+                  options={{
+                    title: "Home"
+                  }}
+                />
+              </Stack>
+            </PaperProvider>
+          </ThemeProvider>
       </QueryClientProvider>
     </ClerkProvider>
   ) 
--- components/OrdersList.tsx
+++ components/OrdersList.tsx
@@ -1,5 +1,16 @@
-import React from "react";
-import { FlatList, Image, StyleSheet, Text, View } from "react-native";
+import useTheme from "@/hooks/useTheme";
+import React, { useState } from "react";
+import {
+  FlatList,
+  Image,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import { Button, Card, Divider, Modal, Portal } from "react-native-paper";
+
 
 type OrderItem = {
   product: { name: string; image: string[] };
@@ -21,78 +32,178 @@ type Order = {
   status?: string;
 };
 
-const OrdersList = ({
-  orders,
-  currency = "₱",
-}: {
-  orders: Order[];
-  currency?: string;
-}) => {
+const OrdersList = ({ orders, currency = "₱" }: { orders: Order[]; currency?: string }) => {
+  const [visible, setVisible] = useState(false);
+  const [selectedOrder, setSelectedOrder] = useState<Order | null>(null);
+  const {colors} = useTheme();
+
+  const openOrderDetails = (order: Order) => {
+    setSelectedOrder(order);
+    setVisible(true);
+  };
+
   const renderOrder = ({ item }: { item: Order }) => {
     return (
-      <View style={styles.orderCard}>
-        {/* Product info */}
+      <TouchableOpacity
+        style={[styles.orderCard, { borderBottomColor: colors.textMuted }]}
+        onPress={() => openOrderDetails(item)}
+      >
         <View style={styles.productSection}>
           <Image
             source={{ uri: item.items[0].product.image[0] }}
             style={styles.productImage}
             resizeMode="cover"
           />
-          <View style={styles.productText}>
-            <View>
-              
-              <Text style={styles.productNames}>
-                {item.items
-                  .map((i) => `${i.product.name} x ${i.quantity}`)
-                  .join(", ")}
-              </Text>
+
+          <View style={{ flex: 1 }}>
+            <Text style={[styles.productNames, { color: colors.text }]}>
+              {item.items.map((i) => `${i.product.name} x ${i.quantity}`).join(", ")}
+            </Text>
+
             <View style={styles.justifiedSection}>
-            <Text>Items: {item.items.length}</Text>
-          <Text style={styles.amountText}>
-            {currency}
-            {item.amount}
-          </Text>
-        </View>
+              <Text style={{ color: colors.text }}>
+                Items: {item.items.length}
+              </Text>
+
+              <Text style={[styles.amountText, { color: colors.primary }]}>
+                {currency}
+                {item.amount}
+              </Text>
             </View>
-            
           </View>
         </View>
-
-        {/* Address */}
-        {/* <View style={styles.addressSection}>
-          <Text>
-            <Text style={styles.addressName}>{item.address?.fullName}</Text>
-            {"\n"}
-            {item.address?.area}
-            {"\n"}
-            {`${item.address?.city}, ${item.address?.province}`}
-            {"\n"}
-            {item.address?.phoneNumber}
-          </Text>
-        </View> */}
-
-        {/* Amount */}
-        
-
-        {/* Payment & Date */}
-        {/* <View style={styles.paymentSection}>
-          <Text>
-            <Text>Method: COD{"\n"}</Text>
-            <Text>Date: {new Date(item.date).toLocaleDateString()}{"\n"}</Text>
-            <Text>Payment: {item.status || "Pending"}</Text>
-          </Text>
-        </View> */}
-      </View>
+      </TouchableOpacity>
     );
   };
 
   return (
-    <FlatList
-      data={orders}
-      renderItem={renderOrder}
-      keyExtractor={(item) => item._id}
-      contentContainerStyle={styles.container}
-    />
+    <>
+      <FlatList
+        data={orders}
+        renderItem={renderOrder}
+        keyExtractor={(item) => item._id}
+        contentContainerStyle={styles.container}
+      />
+
+      {/* Modal */}
+      <Portal>
+        <Modal
+          visible={visible}
+          onDismiss={() => setVisible(false)}
+          contentContainerStyle={[
+            styles.paperModal,
+          ]}
+        >
+          {selectedOrder && (
+            <Card
+              style={[
+                styles.card,
+                { backgroundColor: colors.surface },
+              ]}
+            >
+              <Card.Title
+                title="Order Details"
+                titleStyle={{ fontSize: 20, color: colors.text }}
+              />
+
+              <Card.Content>
+                <ScrollView showsVerticalScrollIndicator={false}>
+                  {/* Items */}
+                  <Text
+                    style={[
+                      styles.sectionHeader,
+                      { color: colors.text }
+                    ]}
+                  >
+                    Items
+                  </Text>
+
+                  {selectedOrder.items.map((i, index) => (
+                    <Text
+                      key={index}
+                      style={[styles.itemText, { color: colors.text }]}
+                    >
+                      • {i.product.name} x {i.quantity}
+                    </Text>
+                  ))}
+
+                  <Divider style={{ marginVertical: 15 }} />
+
+                  {/* Address */}
+                  <Text
+                    style={[
+                      styles.sectionHeader,
+                      { color: colors.text }
+                    ]}
+                  >
+                    Shipping Address
+                  </Text>
+
+                  <Text style={[styles.text, { color: colors.text }]}>
+                    <Text style={styles.bold}>
+                      {selectedOrder.address.fullName}
+                    </Text>
+                    {"\n"}
+                    {selectedOrder.address.area}
+                    {"\n"}
+                    {selectedOrder.address.city}, {selectedOrder.address.province}
+                    {"\n"}
+                    {selectedOrder.address.phoneNumber}
+                  </Text>
+
+                  <Divider style={{ marginVertical: 15 }} />
+
+                  {/* Payment */}
+                  <Text
+                    style={[
+                      styles.sectionHeader,
+                      { color: colors.text }
+                    ]}
+                  >
+                    Payment & Status
+                  </Text>
+
+                  <Text style={[styles.text, { color: colors.text }]}>
+                    Method: COD{"\n"}
+                    Date: {new Date(selectedOrder.date).toLocaleDateString()}
+                    {"\n"}
+                    Payment: {selectedOrder.status || "Pending"}
+                  </Text>
+
+                  <Divider style={{ marginVertical: 15 }} />
+
+                  {/* Amount */}
+                  <Text
+                    style={[
+                      styles.sectionHeader,
+                      { color: colors.text }
+                    ]}
+                  >
+                    Total Amount
+                  </Text>
+
+                  <Text
+                    style={[
+                      styles.amountText,
+                      { color: colors.primary }
+                    ]}
+                  >
+                    {currency}
+                    {selectedOrder.amount}
+                  </Text>
+                </ScrollView>
+              </Card.Content>
+
+              <Card.Actions style={{ justifyContent: "center", marginTop: 10 }}>
+                <Button mode="contained" onPress={() => setVisible(false)} style={{backgroundColor: colors.primary}}>
+                  <Text style={{color: "white"}}>Close</Text>
+                </Button>
+              </Card.Actions>
+            </Card>
+          )}
+        </Modal>
+      </Portal>
+    </>
   );
 };
 
@@ -103,50 +214,72 @@ const styles = StyleSheet.create({
     paddingVertical: 10,
     paddingHorizontal: 12,
   },
+
   orderCard: {
-    flexDirection: "column",
-    gap: 10,
-    padding: 10,
+    paddingVertical: 12,
+    paddingHorizontal: 6,
     borderBottomWidth: 1,
-    borderBottomColor: "#ccc",
   },
+
   productSection: {
     flexDirection: "row",
     gap: 10,
-    maxWidth: 250,
-    marginBottom: 5,
+    alignItems: "center",
   },
+
   productImage: {
     width: 64,
     height: 64,
-    borderRadius: 8,
+    borderRadius: 10,
   },
-  productText: {
+
+  productNames: {
+    fontWeight: "600",
+    fontSize: 16,
+  },
+
+  justifiedSection: {
+    marginTop: 6,
     flexDirection: "row",
     justifyContent: "space-between",
   },
-  productNames: {
-    fontWeight: "500",
+
+  paperModal: {
+    padding: 30,
+  },
+
+  card: {
+    borderRadius: 16,
+    elevation: 0,          // remove Android shadow
+    shadowColor: "transparent", // remove iOS shadow
+    shadowOffset: { width: 0, height: 0 },
+    shadowOpacity: 0,
+    shadowRadius: 0,
+  },
+
+  sectionHeader: {
+    fontWeight: "700",
     fontSize: 16,
+    marginBottom: 6,
   },
-  addressSection: {
-    marginVertical: 5,
+
+  text: {
+    fontSize: 15,
+    lineHeight: 20,
   },
-  addressName: {
-    fontWeight: "500",
+
+  itemText: {
+    fontSize: 15,
+    marginBottom: 4,
   },
-  justifiedSection: {
-    marginVertical: 5,
-    flexDirection:"row",
-    justifyContent: "space-between",
-    width: "100%",
-    paddingRight:15,
-  },
+
   amountText: {
-    fontWeight: "500",
-    fontSize: 16,
+    fontWeight: "700",
+    fontSize: 18,
+    marginTop: 5,
   },
-  paymentSection: {
-    marginVertical: 5,
+
+  bold: {
+    fontWeight: "700",
   },
 });
